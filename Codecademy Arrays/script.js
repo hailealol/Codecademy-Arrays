0b85e9e38bb997787e10293271c351fbc8e96668
hailealol <70154169+hailealol@users.noreply.github.com>
2021-02-03 12:26:53 -0500

    Add files via upload

---
+++ Codecademy Arrays/script.js
@@ -0,0 +1,18 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+secretMessage.pop();
+console.log(secretMessage.length);
+
+secretMessage.push('to', 'Program');
+
+secretMessage[7] = 'right';
+
+secretMessage.shift();
+secretMessage.unshift('Programming');
+
+secretMessage.splice(6, 4);
+
+secretMessage[6] = 'know,'
+console.log(secretMessage.join(' '));
+
+document.write(secretMessage.join(' '));
